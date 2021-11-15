753ef2c1057b974123062f9efd0a469c39978b59
LJ <LJ@WHIT108_08>
2021-11-15 12:17:53 -0500

    Project1-updated

--- src/Hello.java
+++ src/Hello.java
@@ -16,6 +16,7 @@ public class Hello {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello world!");
+        System.out.println("Hello Java");
     }
     
 }
